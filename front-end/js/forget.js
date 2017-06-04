36dc87ddfd9a46aae2f059fc0f13fc90fe024507
terencehyz <1455723823@qq.com>
2017-06-04 18:55:13 +0800

    update

--- front-end/js/forget.js
+++ front-end/js/forget.js
@@ -5,12 +5,28 @@ var serverIp="http://47.93.31.225/project/";
 var vm = new Vue({
    el:"myForget",
    data:{
-
+       rUser:{
+           email:""
+       },
+       rMessage:{
+           getCode:""
+       }
    },
    watch:{
 
    },
    methods:{
+       getcode:function () {
+           var _this = this;
+           var url = serverIp+""+_this.rUser.email+"&callback=JSON_CALLBACK";
+           this.$http.get(url).then(function (res) {
+               if(res.body){
 
+               }
+               else{
+                   _this.rMessage.getCode="账号不存在，请重新输入！";
+               }
+           })
+       }
    }
 });
