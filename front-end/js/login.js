61f41014956615b1ba86f137f0d86578c73d3dd1
terencehyz <1455723823@qq.com>
2017-06-08 17:48:55 +0800

    update js

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -26,15 +26,15 @@ var vm = new Vue({
         login: function () {
             var _this = this;
             var url = serverIp+"login.php?Email="+_this.userInfo.id+"&Password="+_this.userInfo.pwd+"&callback=JSON_CALLBACK";
-            this.$http.get(url).then(function (res) {
+            this.$http.post(url).then(function (res) {
                 /* res.body是取到的真正PHP返回的内容 */
-                if(res.body=="登陆成功"){
+                if(res.body.response==1){
                     _this.loginMessage="";
                     localStorage.setItem("lUserInfo",res.body);
                     localStorage.setItem("lLoginStatus",true);
                     localStorage.setItem("UserName",_this.id);
                     //路由跳转到query页面
-                    window.location="/view/query.html"
+                    window.location="index.html"
                 }
                 else{
                     _this.loginMessage="用户名或密码错误！请重试";
--- front-end/js/register.js
+++ front-end/js/register.js
@@ -28,21 +28,36 @@ var vm = new Vue({
     methods:{
         register: function () {
             var _this = this;
-            var url = serverIp+"login.php?Email="+_this.userInfo.id+"&Password="+_this.userInfo.pwd+"&callback=JSON_CALLBACK";
-            var url = serverIp +"register.php?School="+_this.userInfo.school+"&Academy="+_this.userInfo.academy+"&Email="+_this.userInfo.email+"&Username"+_this.userInfo+"&callback=JSON_CALLBACK";
-            this.$http.get(url).then(function (res) {
-
+            var url = serverIp +"register.php?School="+_this.userInfo.school+"&Academy="+_this.userInfo.academy+"&Email="+_this.userInfo.email+"&Username="+_this.userInfo.name+"&Password="+_this.userInfo.pwd+"&callback=JSON_CALLBACK";
+            this.$http.post(url).then(function (res) {
+                if(res.body.response==1){
+                    //注册成功，进行跳转
+                    window.location="login.html"
+                }
+                if(res.body.response==0){
+                    _this.registerMessage="系统错误，请稍后再试";
+                }
+                if(res.body.response==-1){
+                    _this.registerMessage="此邮箱已注册，请更换邮箱或直接登录";
+                }
+                if(res.body.response==-2){
+                    _this.registerMessage="验证码错误";
+                }
+                if(res.body.response==-3){
+                    _this.registerMessage="验证码已过期";
+                }
             })
         },
-        getCode:function () {
-            if(vm.userInfo.email!=""){
+        getCode: function () {
+            var _this = this;
+            if(_this.userInfo.email!=""||_this.userInfo.email!=undefined||_this.userInfo.email!=''){
                 //正则表达式判断邮箱是否合法
                 var regInvalid=/(@.*@)|(\.\.)|(@\.)|(\.@)|(^\.)/;
                 var regValid=/^.+\@(\[?)[a-zA-Z0-9\-\.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?)$/;
-                if(!regInvalid.test(this.userInfo.email)&&regValid.test(this.userInfo.email)){
-                    var _this = this;
+                if(!regInvalid.test(_this.userInfo.email)&&regValid.test(_this.userInfo.email)){
                     var url = serverIp + "&callback=JSON_CALLBACK";
-                    this.$http.get(url).then(function (res) {
+                    this.$http.post(url).then(function (res) {
+
                     })
                 }
                 else{
@@ -50,7 +65,7 @@ var vm = new Vue({
                 }
             }
             else{
-                vm.registerMessage="邮箱不能为空"
+                _this.registerMessage="邮箱不能为空"
             }
         }
     }
